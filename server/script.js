2a0e5919499aad2b8153bb9d5b527138d2b6b7d2
Bikram Shahi <bikramshahi@Bikrams-MacBook-Pro.local>
2019-02-18 18:13:45 -0500

    front-end

---
+++ server/script.js
@@ -0,0 +1,10 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URLinput');
+
+convertBtn.addEventListener('click'.anchor()=>}
+    SVGAnimatedNumberList(URL.input.value);
+});
+
+function sendURL(URL){
+    window.location.href ='http://localhost:4000/download?URL=${URL}';
+}
